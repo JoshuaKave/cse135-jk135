de8d131506f94e0ab7aa3811406dd7ca11e3511a
Joshua Kave <joshuakave@Joshuas-MacBook-Air.local>
2026-01-31 15:25:28 -0800

    updated env python file

--- base/public_html/cgi-bin/environment-python.py
+++ base/public_html/cgi-bin/environment-python.py
@@ -5,4 +5,5 @@ import os
 print("Content-Type: text/html\n\n")
 
 for name, value in os.environ.items():
-    print(f"\"{name}\": \"{value}\"\n")
+    print(f"\"{name}\": \"{value}\"")
+    print("\n")
