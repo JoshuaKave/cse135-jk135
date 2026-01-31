24368e7ad601a75c347100e8f4825cb8fdce7c27
Joshua Kave <joshuakave@Joshuas-MacBook-Air.local>
2026-01-31 14:22:32 -0800

    changed php hello html

--- base/public_html/cgi-bin/hello-html-php.php
+++ base/public_html/cgi-bin/hello-html-php.php
@@ -1,5 +1,14 @@
 <?php
-
-echo "Hello World!";
-
+echo "<!DOCTYPE html>";
+echo "<html lang=\"en\">";
+echo "<head>";
+echo "  <meta charset=\"UTF-8\">";
+echo "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
+echo "  <title>Greeting</title>";
+echo "</head>";
+echo "<body>";
+echo "  <h1>Hello World!</h1>";
+echo "  <p>Welcome to my simple PHP greeting page.</p>";
+echo "</body>";
+echo "</html>";
 ?>
