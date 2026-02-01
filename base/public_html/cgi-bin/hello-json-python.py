a9d2d38ec6a7417a19bd975b1af062d8bba1d2c8
Joshua Kave <joshuakave@Joshuas-MacBook-Air.local>
2026-01-31 21:29:51 -0800

    fixed python shebang

--- base/public_html/cgi-bin/hello-json-python.py
+++ base/public_html/cgi-bin/hello-json-python.py
@@ -1,9 +1,8 @@
+#!/usr/bin/python3
 import os
 import json
 from datetime import datetime
 from html import escape
-
-#!/usr/bin/python3
 
 print("Content-Type: application/json; charset=UTF-8")
 print()
@@ -12,9 +11,9 @@ remote_addr = os.environ.get("REMOTE_ADDR", "Unknown")
 generated_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 data = {
-    "message": "Hello, this is Josh, and welcome to my PHP World!",
-    "description": "This is an example of a PHP generated page.",
-    "language": "PHP",
+    "message": "Hello, this is Josh, and welcome to my Python World!",
+    "description": "This is an example of a Python generated page.",
+    "language": "Python",
     "generated_at": generated_at,
     "your_ip": escape(remote_addr),
 }
