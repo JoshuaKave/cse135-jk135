3de2b2306ef55791b4b13fa7945cec9d08ef0058
Joshua Kave <joshuakave@Joshuas-MacBook-Air.local>
2026-02-01 12:09:05 -0800

    changed env display nodejs

--- base/public_html/nodeapp/routes/env.js
+++ base/public_html/nodeapp/routes/env.js
@@ -6,12 +6,6 @@ const router = express.Router();
 router.get("/environment-nodejs", (req, res) => {
   res.json({
     ...common(req),
-    request: {
-      method: req.method,
-      path: req.path,
-      query: req.query,
-      headers: req.headers
-    },
     env: process.env
   });
 });
